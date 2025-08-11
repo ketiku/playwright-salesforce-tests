85c0bf2921794b0016960842fe81b00a90cd45c9
Peace <ketikupeace@outlook.com>
2025-08-10 23:13:15 -0300

    updated home page model

--- src/pages/HomePage.ts
+++ src/pages/HomePage.ts
@@ -1,7 +1,16 @@
-import { Page } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 
 export class HomePage {
-    private readonly loginButtonSelector = '#Login';
+    private readonly salesTitle = 'Sales';
 
     constructor(private readonly page: Page) {}
+
+    async expectSalesTitle() {
+        await expect(this.page.getByTitle(this.salesTitle)).toBeVisible({ timeout: 5000 }).catch(
+            async (error) => {
+                console.error('Sales title not found:', error);
+                throw error;
+            }
+        ).then(() => { console.log('Sales title is visible'); });
+    }
 }
